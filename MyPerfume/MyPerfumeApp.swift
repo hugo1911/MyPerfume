a8ae858d26a9351ad450b7be336cb6e3c6678957
hugo1911 <hugokolbe6a@outlook.com>
2025-01-07 18:54:37 -0800

    Initial Commit

---
+++ MyPerfume/MyPerfumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPerfumeApp.swift
+//  MyPerfume
+//
+//  Created by Hugo Manzano on 07/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MyPerfumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
